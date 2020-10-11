4b8e1d813d607144074c4e327ae60b0cc0dbf1a7
Mukilan S <mukilanind01@gmail.com>
2020-10-11 11:06:38 +0530

    js handling

---
+++ js/currencyHandler.js
@@ -0,0 +1,7 @@
+$.getJSON("test.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+});
+json = 
+export const getCurrencyCodefromCountryCode = function(countryCode){
+	return 
+}
